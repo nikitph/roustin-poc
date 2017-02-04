6e25b04bb6dc916d849f594f51b1b690138b5d46
nikit <nikitph@gmail.com>
2017-02-03 16:48:15 -0800

    update mode

--- App/Containers/Styles/ItemInputStyle.js
+++ App/Containers/Styles/ItemInputStyle.js
@@ -2,7 +2,39 @@
 
 import {StyleSheet} from 'react-native'
 import {ApplicationStyles} from '../../Themes/'
+import Metrics from '../../Themes/Metrics'
+
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  container: {},
+  containertwo: {
+    flex: 1,
+    height: Metrics.screenHeight,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    backgroundColor: '#F2DFAE'
+  },
+  slideContainer: {
+    flex: 0.1,
+    backgroundColor: '#F4B459',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  slide: {
+    flex: 1,
+  },
+  slide1: {
+    backgroundColor: '#F4B459',
+  },
+  slide2: {
+    backgroundColor: '#F4B459',
+  },
+  slide3: {
+    backgroundColor: '#F4B459',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
+  },
 })
