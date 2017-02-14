d350e5e3f20aedce2d057a8854d5d526d3add488
nikit <nikitph@gmail.com>
2017-02-14 09:55:14 -0800

    update mode

--- App/Components/Styles/RoundedButtonStyle.js
+++ App/Components/Styles/RoundedButtonStyle.js
@@ -7,15 +7,17 @@ export default StyleSheet.create({
   button: {
     height: 40,
     borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'transparent',
     marginHorizontal: Metrics.smallMargin,
     marginVertical: Metrics.smallMargin,
-    backgroundColor: '#B18A6A',
+    backgroundColor: 'rgba(177, 138, 106, 0.5)',
     justifyContent: 'center'
   },
   buttonText: {
-    color: '#FCDCB8',
+    color: '#482e19',
     textAlign: 'center',
-    fontWeight: '200',
+    fontWeight: '400',
     fontFamily: 'Avenir',
     fontSize: Fonts.size.small,
     marginHorizontal: Metrics.baseMargin
